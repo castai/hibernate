52d04cbea49e4565802b31012d91cb9e4f0391c9
Viktoras Makauskas <viktoras@cast.ai>
2022-11-18 15:32:38 +0200

    suggested changes

--- app/main.py
+++ app/main.py
@@ -43,24 +43,20 @@ cloud_labels = {
 k8s_v1 = client.CoreV1Api()
 k8s_v1_apps = client.AppsV1Api()
 
-if __name__ == '__main__':
-    logging.info("Hibernation input parameters token: %s, clusterId: %s, cloud: %s, action: %s",
-                 castai_api_token, cluster_id, cloud, action)
 
-    if action == "resume":
-        logging.info("Resuming cluster, autoscaling will be enabled")
-        policy_changed = toggle_unschedulable_pod_policy_enabled(cluster_id, castai_api_token, True)
-        if policy_changed:
-            logging.info("Resume operation completed.")
-            exit(0)
-        else:
-            logging.error("Could not enable CAST AI autoscaler.")
-            exit(1)
+def handle_resume():
+    logging.info("Resuming cluster, autoscaling will be enabled")
+    policy_changed = toggle_unschedulable_pod_policy_enabled(cluster_id, castai_api_token, True)
+    if not policy_changed:
+        raise Exception("could not enable CAST AI autoscaler.")
 
-    policy_changed = toggle_unschedulable_pod_policy_enabled(cluster_id, castai_api_token, False)
+    logging.info("Resume operation completed.")
 
+
+def handle_suspend():
+    toggle_unschedulable_pod_policy_enabled(cluster_id, castai_api_token, False)
     hibernation_node_id = hibernation_node_already_exist(client=k8s_v1, taint=castai_pause_toleration,
-                                                             k8s_label=cloud_labels[cloud])
+                                                         k8s_label=cloud_labels[cloud])
 
     if not hibernation_node_id and cloud == "AKS":
         logging.info("Checking special Azure case if suitable Azure node could be converted")
@@ -74,14 +70,13 @@ if __name__ == '__main__':
         else:
             hibernate_node_size = instance_type[cloud]
         hibernation_node_id = create_hibernation_node(cluster_id, castai_api_token, instance_type=hibernate_node_size,
-                                                          k8s_taint=castai_pause_toleration, cloud=cloud)
+                                                      k8s_taint=castai_pause_toleration, cloud=cloud)
 
     if hibernation_node_id:
         logging.info("Hibernation node exist: %s", hibernation_node_id)
         cordon_all_nodes(k8s_v1, exclude_node_id=hibernation_node_id)
     else:
-        logging.error("No ready hibernation node exist")
-        exit(-1)
+        raise Exception("no ready hibernation node exist")
 
     add_special_tolerations(client=k8s_v1_apps, namespace=kube_system_namespace, toleration=castai_pause_toleration)
     add_special_tolerations(client=k8s_v1_apps, namespace=cast_namespace, toleration=castai_pause_toleration)
@@ -105,3 +100,21 @@ if __name__ == '__main__':
         delete_castai_node(cluster_id, castai_api_token, my_node_name_id)
 
     logging.info("Pause operation completed.")
+
+
+def main():
+    logging.info("Hibernation input parameters token: %s, clusterId: %s, cloud: %s, action: %s",
+                 castai_api_token, cluster_id, cloud, action)
+
+    if action == "resume":
+        return handle_resume()
+
+    handle_suspend()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as err:
+        logging.error("action failed:" + str(err))
+        exit(1)
