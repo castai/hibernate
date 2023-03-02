3f6bb95b918fbeba72d084d5d7810a044c6e0da8
Augustinas Stirbis <49340408+AugustinasS@users.noreply.github.com>
2023-03-02 11:44:02 +0200

    job dies before deleting nodes bug

--- app/k8s_utils.py
+++ app/k8s_utils.py
@@ -6,6 +6,7 @@ class K8sAPIError(Exception):
     pass
 
 
+@basic_retry(attempts=3, pause=10)
 def cordon_all_nodes(client, protect_removal_disabled: str, exclude_node_id: str):
     """cordon all the nodes so essential components could be scheduled only on hybernation node"""
     logging.info("Cordon function")
@@ -19,15 +20,14 @@ def cordon_all_nodes(client, protect_removal_disabled: str, exclude_node_id: str
     node_list = client.list_node()
     for node in node_list.items:
         logging.info("Inspecting node %s to cordon", node.metadata.name)
-        skip = False
-        if node.metadata.labels.get(
-                "autoscaling.cast.ai/removal-disabled") == "true" and protect_removal_disabled == "true":
-            skip = True
-        if node.metadata.labels.get("provisioner.cast.ai/node-id") != exclude_node_id and not skip:
-            logging.info("Cordoning: %s" % node.metadata.name)
-            client.patch_node(node.metadata.name, node_body)
-        else:
-            logging.info("skip Cordoning node: %s" % node.metadata.name)
+        if node.metadata.labels.get("autoscaling.cast.ai/removal-disabled") == "true" and protect_removal_disabled == "true":
+            logging.info("skip Cordoning node: %s due protect_removal_disabled label" % node.metadata.name)
+            continue
+        if node.metadata.labels.get("provisioner.cast.ai/node-id") == exclude_node_id:
+            logging.info("skip Cordoning hibernation node: %s" % node.metadata.name)
+            continue
+        logging.info("Cordoning: %s" % node.metadata.name)
+        client.patch_node(node.metadata.name, node_body)
 
 
 def deployment_tolerates(deployment, toleration):
@@ -38,7 +38,7 @@ def deployment_tolerates(deployment, toleration):
     return False
 
 
-@basic_retry(attempts=2, pause=5)
+@basic_retry(attempts=3, pause=15)
 def add_special_toleration(client, deployment: str, toleration: str):
     """" modify essential deployment to keep them running on hibernation node (tolerate node)"""
     logging.info("add tolerations to essential workloads function")
@@ -84,17 +84,15 @@ def add_special_toleration(client, deployment: str, toleration: str):
 def check_hibernation_node_readiness(client, taint: str, node_name: str):
     """ check if node has taint """
     node = client.read_node(node_name)
-
-    if node.spec.taints:
-        for current_taint in node.spec.taints:
-            if current_taint.to_dict()["key"] == taint:
-                if node_is_ready(node):
-                    logging.info("found tainted hibernation node %s", node_name)
-                    return node.metadata.labels.get("provisioner.cast.ai/node-id")
+    if check_if_node_has_specific_taint(client, taint, node_name):
+        if node_is_ready(node):
+            logging.info("found tainted hibernation node %s", node_name)
+            return node.metadata.labels.get("provisioner.cast.ai/node-id")
     logging.info("Hibernation node %s is not READY %s", node_name)
     return None
 
 
+@basic_retry(attempts=3, pause=10)
 def node_is_ready(node: str):
     """ check if node is ready """
     node_scheduling = node.spec.unschedulable
@@ -103,6 +101,17 @@ def node_is_ready(node: str):
             return True
     return False
 
+def check_if_node_has_specific_taint(client, taint: str, node_name: str):
+    """ check if node has taint """
+    node = client.read_node(node_name)
+
+    if node.spec.taints:
+        for current_taint in node.spec.taints:
+            if current_taint.to_dict()["key"] == taint:
+                logging.info("found tainted node %s", node_name)
+                return True
+    logging.info("Node %s is not tainted", node_name)
+    return False
 
 @basic_retry(attempts=2, pause=5)
 def add_node_taint(client, pause_taint: str, node_name):
@@ -116,6 +125,10 @@ def add_node_taint(client, pause_taint: str, node_name):
     current_taints = node.spec.taints
     if current_taints is None:
         current_taints = []
+    elif check_if_node_has_specific_taint(client, pause_taint, node_name):
+        logging.info(f'node {node_name} already has {pause_taint} taint')
+        return
+
     current_taints.append(taint_to_add)
 
     taint_body = {
@@ -140,7 +153,7 @@ def add_node_taint(client, pause_taint: str, node_name):
         logging.info("node %s successfully patched", node.metadata.name)
 
 
-@basic_retry(attempts=2, pause=5)
+@basic_retry(attempts=3, pause=5)
 def remove_node_taint(client, pause_taint: str, node_id: str):
     """ remove specific taint from node"""
     node = client.list_node(label_selector=f"provisioner.cast.ai/node-id={node_id}").items[0]
@@ -164,7 +177,7 @@ def remove_node_taint(client, pause_taint: str, node_id: str):
         logging.error(f'failed to patch node {node_name}, with details {patch_result}')
 
 
-@basic_retry(attempts=2, pause=5)
+@basic_retry(attempts=3, pause=15)
 def get_node_castai_id(client, node_name: str):
     """" Node with hibernation taint already exist """
     k8s_label = "kubernetes.io/hostname=" + node_name
@@ -176,6 +189,7 @@ def get_node_castai_id(client, node_name: str):
             return node_id
 
 
+@basic_retry(attempts=3, pause=15)
 def get_deployments_names_with_system_priority_class(client):
     """Return all system-critical priority-class deployments"""
     deployments = client.list_deployment_for_all_namespaces()
@@ -185,6 +199,7 @@ def get_deployments_names_with_system_priority_class(client):
     return critical_deploys
 
 
+@basic_retry(attempts=3, pause=15)
 def has_system_priority_class(deployment):
     """ validate if Deployment is system critical"""
     if deployment.spec.template.spec.priority_class_name in ("system-cluster-critical", "system-node-critical"):
