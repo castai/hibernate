c1ce9c12781bd0a58db17f7d52d99e65222fb17c
Augustinas Stirbis <49340408+AugustinasS@users.noreply.github.com>
2024-05-14 17:19:14 +0300

    Wait for AKS CNI to start ConfigMap not found better handling

--- hack/aks/castai.tf
+++ hack/aks/castai.tf
@@ -4,6 +4,7 @@
 data "azurerm_subscription" "current" {}
 
 provider "castai" {
+  api_url   = var.castai_api_url
   api_token = var.castai_api_token
 }
 
@@ -20,6 +21,11 @@ provider "helm" {
 module "castai-aks-cluster" {
   source = "castai/aks/castai"
 
+  api_url                = var.castai_api_url
+  castai_api_token       = var.castai_api_token
+  grpc_url               = var.castai_grpc_url
+  wait_for_cluster_ready = true
+
   aks_cluster_name    = var.cluster_name
   aks_cluster_region  = var.cluster_region
   node_resource_group = azurerm_kubernetes_cluster.this.node_resource_group
@@ -34,30 +40,98 @@ module "castai-aks-cluster" {
 
   node_configurations = {
     default = {
-      disk_cpu_ratio  = 25
-      subnets         = [azurerm_subnet.internal.id]
-      tags            = var.tags
+      disk_cpu_ratio = 25
+      subnets        = [azurerm_subnet.internal.id]
+      tags           = var.tags
+    }
+
+    test_node_config = {
+      disk_cpu_ratio    = 25
+      subnets           = [azurerm_subnet.internal.id]
+      tags              = var.tags
+      max_pods_per_node = 40
     }
   }
 
+  node_templates = {
+    default_by_castai = {
+      name             = "default-by-castai"
+      configuration_id = module.castai-aks-cluster.castai_node_configurations["default"]
+      is_default       = true
+      is_enabled       = true
+      should_taint     = false
+
+      constraints = {
+        on_demand          = true
+        spot               = true
+        use_spot_fallbacks = true
+
+        enable_spot_diversity                       = false
+        spot_diversity_price_increase_limit_percent = 20
+      }
+    }
+    spot_tmpl = {
+      configuration_id = module.castai-aks-cluster.castai_node_configurations["default"]
+      is_enabled       = true
+      should_taint     = true
+
+      custom_labels = {
+        custom-label-key-1 = "custom-label-value-1"
+        custom-label-key-2 = "custom-label-value-2"
+      }
+
+      custom_taints = [
+        {
+          key   = "custom-taint-key-1"
+          value = "custom-taint-value-1"
+        },
+        {
+          key   = "custom-taint-key-2"
+          value = "custom-taint-value-2"
+        }
+      ]
+
+      constraints = {
+        fallback_restore_rate_seconds = 1800
+        spot                          = true
+        use_spot_fallbacks            = true
+        min_cpu                       = 4
+        max_cpu                       = 100
+        instance_families = {
+          exclude = ["standard_DPLSv5"]
+        }
+        compute_optimized = false
+        storage_optimized = false
+
+        # Optional: define custom priority for instances selection.
+        #
+        # 1. Prioritize Standard_Fs spot instances above all else, regardless of price.
+        # 2. If Standard_FS is not available, try Standard_FXMDS family.
+        custom_priority = [
+          {
+            instance_families = ["Standard_FS"]
+            spot              = true
+          },
+          {
+            instance_families = ["Standard_FXMDS"]
+            spot              = true
+          }
+          # 3. instances not matching any of custom priority groups will be tried after
+          # nothing matches from priority groups.
+        ]
+      }
+    }
+  }
 
   // Configure Autoscaler policies as per API specification https://api.cast.ai/v1/spec/#/PoliciesAPI/PoliciesAPIUpsertClusterPolicies.
   // Here:
   //  - unschedulablePods - Unscheduled pods policy
-  //  - spotInstances     - Spot instances configuration
   //  - nodeDownscaler    - Node deletion policy
-  autoscaler_policies_json   = <<-EOT
+  autoscaler_policies_json = <<-EOT
     {
         "enabled": true,
         "unschedulablePods": {
             "enabled": true
-        },
-        "spotInstances": {
-            "enabled": true,
-            "clouds": ["azure"],
-            "spotBackups": {
-                "enabled": true
-            }
         },
         "nodeDownscaler": {
             "enabled": true,
@@ -73,6 +147,7 @@ module "castai-aks-cluster" {
                 "scopedMode": false
             }
         },
+        "nodeTemplatesPartialMatchingEnabled": false,
         "clusterLimits": {
             "cpu": {
                 "maxCores": 20,
